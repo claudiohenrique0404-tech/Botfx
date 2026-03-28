e86f68974214ea5eeb7894e81bfab1f50ce10bd4
claudiohenrique0404-tech <claudiohenrique0404@icloud.com>
2026-03-28 18:08:36 +0000

    Update db-update.js

--- api/db-update.js
+++ api/db-update.js
@@ -2,14 +2,9 @@ const { updateTradeResult } = require('./db');
 
 module.exports = async (req,res)=>{
 
-  try{
-    const { symbol, pnl } = req.body;
+  const { symbol, pnl } = req.body;
 
-    updateTradeResult(symbol, pnl);
+  updateTradeResult(symbol, pnl);
 
-    res.json({ok:true});
-
-  }catch(e){
-    res.status(500).json({error:e.message});
-  }
+  res.json({ok:true});
 };
