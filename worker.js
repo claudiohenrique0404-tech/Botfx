f2e126ccc296df4175fbad1d9e3eb8e2a0570005
claudiohenrique0404-tech <claudiohenrique0404@icloud.com>
2026-03-28 22:43:37 +0000

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,1 +1,21 @@
+const runBot = require('./cron');
 
+function sleep(ms){
+  return new Promise(r => setTimeout(r, ms));
+}
+
+async function start(){
+  console.log("🚀 BOT STARTED");
+
+  while(true){
+    try{
+      await runBot();
+    }catch(e){
+      console.log("ERROR:", e.message);
+    }
+
+    await sleep(5000);
+  }
+}
+
+start();
