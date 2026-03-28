1f89841da1918f1efb42dc06260ea924491de762
claudiohenrique0404-tech <claudiohenrique0404@icloud.com>
2026-03-28 18:06:53 +0000

    Update ml-client.js

--- api/ml-client.js
+++ api/ml-client.js
@@ -1,12 +1,21 @@
 async function getPrediction(data){
 
-  const r = await fetch('https://ml-api-production.up.railway.app/predict',{
-    method:'POST',
-    headers:{'Content-Type':'application/json'},
-    body: JSON.stringify({ data })
-  });
+  try{
 
-  return await r.json();
+    const r = await fetch('https://ml-api-production-d47c.up.railway.app/predict',{
+      method:'POST',
+      headers:{'Content-Type':'application/json'},
+      body: JSON.stringify({ data })
+    });
+
+    const d = await r.json();
+
+    return d;
+
+  }catch(e){
+    console.log('ML ERROR:', e.message);
+    return null;
+  }
 }
 
 module.exports = { getPrediction };
