d44711f96c96ee5ab6e1677a54f3faed6f1fa59c
claudiohenrique0404-tech <claudiohenrique0404@icloud.com>
2026-04-06 15:43:31 +0100

    Update bitget.js

--- api/bitget.js
+++ api/bitget.js
@@ -9,7 +9,7 @@ if (!global.BOT_SETTINGS) {
   global.BOT_SETTINGS = {
     active: true,
     risk: 1,
-    lev: 10,
+    lev: 5,
     symbols: [
       'BTCUSDT','ETHUSDT','SOLUSDT','XRPUSDT',
       'BNBUSDT','ADAUSDT','AVAXUSDT','LINKUSDT',
